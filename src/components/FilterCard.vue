b577757be314fec28fc5bc00b4eb15db3bb88584
yemson <lier@kakao.com>
2023-04-05 22:18:26 +0900

    feat: 필터 카드에 이미지 추가

--- src/components/FilterCard.vue
+++ src/components/FilterCard.vue
@@ -20,23 +20,40 @@ defineProps({
         <ChevronIcon />
       </button>
     </div>
-    <div class="carousel space-x-2 mt-2">
+    <div class="carousel space-x-3 mt-2">
       <div
         v-for="i in 10"
         :key="i"
         class="carousel-item"
       >
-        <div class="card card-compact bg-base-100 w-48 h-48 border">
+        <div class="card card-compact w-48 h-52 bg-base-100 image-full">
+          <figure>
+            <img
+              src="https://picsum.photos/seed/picsum/200/300"
+              alt="Picture"
+              class="object-cover w-48 h-52"
+            >
+          </figure>
           <div class="card-body">
             <h2 class="card-title">
               Shoes!
             </h2>
-            <p class="text-sm">
-              If a dog chews shoes whose shoes does he choose?
-            </p>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
           </div>
         </div>
       </div> 
     </div>
   </div>
 </template>
+
+<style scoped>
+.card.image-full:before {
+    position: relative;
+    content: "";
+    z-index: 10;
+    --tw-bg-opacity: 0.4;
+    background-color: hsl(var(--n) / var(--tw-bg-opacity));
+    opacity: 0.75;
+    border-radius: var(--rounded-box, 1rem);
+}
+</style>
